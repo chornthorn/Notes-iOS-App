afd87895e11ee1277cf8cadeb4b6ec5d3203104f
Chorn Thron <chornthorn.kh@gmail.com>
2023-02-02 18:47:02 +0700

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by thorn on 2/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
